f16f48b2383e0a13a1c03881fb9bf004513ecf5e
KoreanHordes <116519117+KoreanHordes@users.noreply.github.com>
2022-10-25 23:46:47 +0200

    Update pet.js
    korean translation

--- loc/items/pet.js
+++ loc/items/pet.js
@@ -6,6 +6,7 @@ export default [
       en: 'A tiny grub that will loot items for you.',
       ru: 'Крошечная личинка, которая будет подбирать для вас предметы.',
       fr: 'Un petit asticot qui ramassera les objets pour vous.',
+      ko: '당신을 위해 자동으로 아이템을 줍는 작은 에벌레입니다.',
       tr: 'Eşyaları senin için toplayan minik bir kurtçuk.'
     }
   },
@@ -17,6 +18,7 @@ export default [
       en: 'A tiny goblin that will loot items for you.',
       ru: 'Крошечный гоблин, который будет подбирать для вас предметы.',
       fr: 'Un petit gobelin qui ramassera les objets pour vous.',
+      ko: '당신을 위해 자동으로 아이템을 줍는 작은 고블린입니다.',
       tr: 'Eşyaları senin için toplayan minik bir goblin.'
     }
   }
